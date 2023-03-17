8b3b8726083e11381c50e266a17b1892f0914246
Shira Sowen <shi@shidigital.com>
2023-03-17 22:00:45 +0000

    day 6

--- projekte/guess_number_game/guess_the_number_game_gui.py
+++ projekte/guess_number_game/guess_the_number_game_gui.py
@@ -6,12 +6,10 @@ import tkinter as tk
 def play():
     # print Starting game...
     start_msg = "Start of Game"
-    print(start_msg)
     writeToLog(start_msg)
 
     # print Player Turn
     turn_msg = "Player Turn"
-    print(turn_msg)
     writeToLog(turn_msg)
 
     # change Play Button to "Player Turn"
@@ -24,7 +22,6 @@ def play():
 
     # print Computer Turn
     turn_msg = "Computer Turn"
-    print(turn_msg)
     writeToLog(turn_msg)
 
     # change Play Button to "Computer Turn" 
@@ -43,12 +40,10 @@ def play():
     else:
         result_msg = f"Player: {player_guesses} Computer: {computer_guesses} \n The Computer wins this Game!"
 
-    print(result_msg)
     writeToLog(result_msg)    
 
     # print End of game
     start_msg = "End of Game \n"
-    print(start_msg)
     writeToLog(start_msg)
 
     # change Play Button to "Play"
@@ -70,17 +65,71 @@ def writeToLog(msg):
     log_txt.insert('end', msg)
     log_txt["state"] = 'disabled'
 
+    print(msg) # check in console
 
+
+# Player Turn Function
 def player_turn():
     print("Player Turn Function")
-    guesses = input("Number? > ")
+
+    to_log = f"The computer chooses a secret number between 1 and 100 ..."
+    writeToLog(to_log)
+
+    computer_number = random.randint(1, 100)
+    writeToLog(f"Secret Number: {computer_number}")
+
+    guesses = 0
+    guess = 0
+
+
+    while guess != computer_number:
+
+        player_guess_btn['state'] = 'normal'
+        guess = player_guess_btn_pressed()
+
+        # user_input = input("Try to guess the number? > ")
+        guesses += 1
+
+        
+        if guess > computer_number:
+            writeToLog("Your number is to high!")
+
+        else:
+            writeToLog("Your number is too low!")
+
+
+    writeToLog(f"You guessed the number {guess} correct. You needed {guesses} guesses.")
+
+    player_guess_btn['state'] = 'disabled'
+
+    # guesses = input("Number? > ")
     writeToLog(f"Player Guesses: {guesses}")
     return guesses
 
 
+# PROBLEM HIER
+# Player Guess Btn Press
+def player_guess_btn_pressed():
+    '''
+    if player_guess_ent.get() != '':
+        guess = int(player_guess_ent.get())
+        writeToLog(guess)
+        return int(guess)
+    else:
+        print("Something is wrong.")
+        player_guess_ent['text'] = '0'
+    '''
+
+    guess = int(input("Test Input >> "))
+    return guess
+
+    
+
+
+# Computer Turn Function
 def computer_turn():
     print("Computer Turn Function")
-    guesses = input("Number? > ")
+    guesses = int(input("Number? > "))
     writeToLog(f"Computer Guesses: {guesses}")
     return guesses
 
@@ -154,11 +203,14 @@ player_name_lbl.pack(pady=2, padx=2)
 
 player_guess_ent = tk.Entry(
     master=player_frm,
-    width=5)
+    width=5,
+    text='0')
 player_guess_ent.pack(pady=2, padx=2)
 
 player_guess_btn = tk.Button(
     master=player_frm,
+    command=player_guess_btn_pressed,
+    state="disabled",
     text="Is this the secret number?")
 player_guess_btn.pack(pady=2, padx=2)
 
